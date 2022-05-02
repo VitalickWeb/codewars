14cbe435232f1b4f55b78c79f7c29e0db691c128
Vitalick Bokhan <miraigeweb@gmail.com>
2022-05-02 16:53:51 +0300

    created a function fakeBin

--- wars01.js
+++ wars01.js
@@ -57,3 +57,20 @@
 // console.log(positiveSum([-1, -2, -3, -4, -5]));//, 0
 // console.log(positiveSum([-1, 2, 3, 4, -5]));//, 9
 
+//Фальшивая корзина
+function fakeBin(x) {
+	let result = '';
+
+	for (let elem of x) {
+		if (elem < 5) {
+			result += '0';
+		} else if (elem >= 5) {
+			result += '1';
+		}
+	}
+	return result;
+}
+
+console.log(fakeBin('45385593107843568'));//, '01011110001100111'
+console.log(fakeBin('509321967506747'));//, '101000111101101' 
+console.log(fakeBin('366058562030849490134388085'));//, '011011110000101010000011011'
