46863935d5f5db6c5f58bfe818aa60bd69e8a33a
Vitalick Bokhan <miraigeweb@gmail.com>
2022-05-09 23:47:51 +0300

    not work

--- wars01.js
+++ wars01.js
@@ -413,23 +413,37 @@
 // console.log(numberToString(67), '67');
 
 
-function betterThanAverage(classPoints, yourPoints) {
-	classPoints.push(yourPoints);
-	let sumPoints = 0;
 
-	for (let i = 0; i < classPoints.length; i++) {
-		sumPoints += classPoints[i];
-	}
+// function betterThanAverage(classPoints, yourPoints) {
+// 	classPoints.push(yourPoints);
+// 	let sumPoints = 0;
 
-	if (yourPoints > sumPoints / classPoints.length) {
-		return true;
-	} else {
-		return false;
-	}
+// 	for (let i = 0; i < classPoints.length; i++) {
+// 		sumPoints += classPoints[i];
+// 	}
+
+// 	if (yourPoints > sumPoints / classPoints.length) {
+// 		return true;
+// 	} else {
+// 		return false;
+// 	}
+// }
+
+// console.log(betterThanAverage([2, 3], 5), true);
+// console.log(betterThanAverage([100, 40, 34, 57, 29, 72, 57, 88], 75), true);
+// console.log(betterThanAverage([12, 23, 34, 45, 56, 67, 78, 89, 90], 9), false);
+// console.log(betterThanAverage([41, 75, 72, 56, 80, 82, 81, 33], 50), false);
+// console.log(betterThanAverage([29, 55, 74, 60, 11, 90, 67, 28], 21), false);
+
+
+
+function getMiddle(s) {
+	let arr = s.split('');
+	console.log(arr)
+	//return arr;
 }
 
-console.log(betterThanAverage([2, 3], 5), true);
-console.log(betterThanAverage([100, 40, 34, 57, 29, 72, 57, 88], 75), true);
-console.log(betterThanAverage([12, 23, 34, 45, 56, 67, 78, 89, 90], 9), false);
-console.log(betterThanAverage([41, 75, 72, 56, 80, 82, 81, 33], 50), false);
-console.log(betterThanAverage([29, 55, 74, 60, 11, 90, 67, 28], 21), false);
+console.log(getMiddle("test"), "es");
+console.log(getMiddle("testing"), "t");
+console.log(getMiddle("middle"), "dd");
+console.log(getMiddle("A"), "A");
