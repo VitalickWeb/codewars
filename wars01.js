2612fe5ed0d1cd412534178c81efebedee19ebd3
Vitalick Bokhan <miraigeweb@gmail.com>
2022-05-11 23:34:54 +0300

    reverse string

--- wars01.js
+++ wars01.js
@@ -468,27 +468,28 @@
 
 
 
-function bmi(weight, height) {
-	let bmi = weight / (height * height);
-	if (bmi <= 18.5) {
-		return 'Underweight';
-	} else if (bmi <= 25.0) {
-		return 'Normal';
-	} else if (bmi <= 30.0) {
-		return "Overweight";
-	} else if (bmi > 30) {
-		return "Obese";
-	}
+// function bmi(weight, height) {
+// 	let bmi = weight / (height * height);
+// 	if (bmi <= 18.5) {
+// 		return 'Underweight';
+// 	} else if (bmi <= 25.0) {
+// 		return 'Normal';
+// 	} else if (bmi <= 30.0) {
+// 		return "Overweight";
+// 	} else if (bmi > 30) {
+// 		return "Obese";
+// 	}
+// }
+
+// console.log(bmi(80, 1.80), "Normal");
+
+
+
+function solution(str) {
+	return str.split('').reverse().join('');
 }
 
-console.log(bmi(80, 1.80), "Normal");
-//(bmi = вес / рост2).
-
-
-//if bmi <= 18.5 return "Underweight"
-
-//if bmi <= 25.0 return "Normal"
-
-//if bmi <= 30.0 return "Overweight"
-
-//if bmi > 30 return "Obese"
+console.log(solution('world'), 'dlrow');
+console.log(solution('hello'), 'olleh');
+console.log(solution(''), '');
+console.log(solution('h'), 'h');
