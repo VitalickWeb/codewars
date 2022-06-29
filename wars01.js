e82af32306158dceed7e0181b1853b2881a9f16c
Vitalick Bokhan <miraigeweb@gmail.com>
2022-06-29 22:09:10 +0300

    summury min nums

--- wars01.js
+++ wars01.js
@@ -673,11 +673,26 @@
 
 
 
-function lovefunc(flower1, flower2) {
-	return flower1 % 2 !== 0 && flower2 % 2 === 0 || flower1 % 2 === 0 && flower2 % 2 !== 0;
+// function lovefunc(flower1, flower2) {
+// 	return flower1 % 2 !== 0 && flower2 % 2 === 0 || flower1 % 2 === 0 && flower2 % 2 !== 0;
+// }
+
+// console.log(lovefunc(1, 4), true)
+// console.log(lovefunc(2, 2), false)
+// console.log(lovefunc(0, 1), true)
+// console.log(lovefunc(0, 0), false)
+
+
+
+function sumTwoSmallestNumbers(numbers) {
+	let newArr = numbers.sort((a, b) => a - b)
+	let res = newArr.slice(0, 2);
+	return res.reduce((acc, el) => acc + el)
+	//return res.map((r, i) => r + res[i + 1])[0];
 }
 
-console.log(lovefunc(1, 4), true)
-console.log(lovefunc(2, 2), false)
-console.log(lovefunc(0, 1), true)
-console.log(lovefunc(0, 0), false)
+console.log(sumTwoSmallestNumbers([5, 8, 12, 19, 22]), 13, "Sum should be 13");
+console.log(sumTwoSmallestNumbers([15, 28, 4, 2, 43]), 6, "Sum should be 6");
+console.log(sumTwoSmallestNumbers([3, 87, 45, 12, 7]), 10, "Sum should be 10");
+console.log(sumTwoSmallestNumbers([23, 71, 33, 82, 1]), 24, "Sum should be 24");
+console.log(sumTwoSmallestNumbers([52, 76, 14, 12, 4]), 16, "Sum should be 16");
